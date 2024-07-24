c25867e47a4c44f76ccf80ba6c33f95bc9ca83d8
Малахов Алексей Павлович <a.p.malakhov@tinkoff.ru>
2024-07-24 14:30:52 +0300

    update tutorials

---
+++ tutorials/rm/rm.py
@@ -0,0 +1,42 @@
+from datasets import load_dataset
+from turbo_alignment.dataset.pair_preferences.models import PairPreferenceRecord
+
+from turbo_alignment.common.data.io import write_jsonl
+from pathlib import Path
+
+import subprocess
+
+
+def convert_to_preference_record(row: dict[str, str | int]) -> PairPreferenceRecord:
+    assert row['messages'][:-1] == row['chosen'][:-1]
+    assert row['messages'][:-1] == row['rejected'][:-1]
+
+    for msg in row['messages']:
+        if msg['role'] == 'assistant':
+            msg['role'] = 'bot'
+    
+    row['chosen'][-1]['role'] = 'bot'
+    row['rejected'][-1]['role'] = 'bot'
+
+    return PairPreferenceRecord(
+        id=row['prompt_id'],
+        context=row['messages'][:-1],
+        answer_w=row['chosen'][-1],
+        answer_l=row['rejected'][-1],
+    ).dict()
+
+
+
+if __name__ == "__main__":
+    ds = load_dataset("HuggingFaceH4/ultrafeedback_binarized")
+
+    ds['train_pair_preference_records'] = ds['train_prefs'].map(convert_to_preference_record, remove_columns=ds['train_prefs'].column_names)
+    ds['val_pair_preference_records'] = ds['test_prefs'].map(convert_to_preference_record, remove_columns=ds['test_prefs'].column_names)
+
+
+    write_jsonl([item for item in ds['train_pair_preference_records']], Path('train_preferences.jsonl'))
+    write_jsonl([item for item in ds['val_pair_preference_records']], Path('val_preferences.jsonl'))
+
+
+    launch_code = "poetry run python -m turbo_alignment train_rm --experiment_settings_path turbo-alignment/tutorials/rm/rm.json"
+    subprocess.run(launch_code, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
