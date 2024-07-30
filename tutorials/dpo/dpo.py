aaeee2364794e4feba006210689b0328c7e458a7
Малахов Алексей Павлович <a.p.malakhov@tinkoff.ru>
2024-07-30 15:10:30 +0000

    update tutorials code

--- tutorials/dpo/dpo.py
+++ tutorials/dpo/dpo.py
@@ -1,11 +1,11 @@
-from datasets import load_dataset
-from turbo_alignment.dataset.pair_preferences.models import PairPreferenceRecord
-from turbo_alignment.dataset.chat.chat import ChatDatasetRecord
-
-from turbo_alignment.common.data.io import write_jsonl
+import subprocess
 from pathlib import Path
 
-import subprocess
+from datasets import load_dataset
+
+from turbo_alignment.common.data.io import write_jsonl
+from turbo_alignment.dataset.chat.chat import ChatDatasetRecord
+from turbo_alignment.dataset.pair_preferences.models import PairPreferenceRecord
 
 
 def convert_to_preference_record(row: dict[str, str | int]) -> PairPreferenceRecord:
@@ -15,7 +15,7 @@ def convert_to_preference_record(row: dict[str, str | int]) -> PairPreferenceRec
     for msg in row['messages']:
         if msg['role'] == 'assistant':
             msg['role'] = 'bot'
-    
+
     row['chosen'][-1]['role'] = 'bot'
     row['rejected'][-1]['role'] = 'bot'
 
@@ -38,18 +38,20 @@ def convert_to_chat_record(row: dict[str, str | int]) -> ChatDatasetRecord:
     ).dict()
 
 
-if __name__ == "__main__":
-    ds = load_dataset("HuggingFaceH4/ultrafeedback_binarized")
+if __name__ == '__main__':
+    ds = load_dataset('HuggingFaceH4/ultrafeedback_binarized')
 
-    ds['train_pair_preference_records'] = ds['train_prefs'].map(convert_to_preference_record, remove_columns=ds['train_prefs'].column_names)
-    ds['val_pair_preference_records'] = ds['test_prefs'].map(convert_to_preference_record, remove_columns=ds['test_prefs'].column_names)
+    ds['train_pair_preference_records'] = ds['train_prefs'].map(
+        convert_to_preference_record, remove_columns=ds['train_prefs'].column_names
+    )
+    ds['val_pair_preference_records'] = ds['test_prefs'].map(
+        convert_to_preference_record, remove_columns=ds['test_prefs'].column_names
+    )
     ds['val_chat_records'] = ds['test_prefs'].map(convert_to_chat_record, remove_columns=ds['test_prefs'].column_names)
-
 
     write_jsonl([item for item in ds['train_pair_preference_records']], Path('train_preferences.jsonl'))
     write_jsonl([item for item in ds['val_pair_preference_records']], Path('val_preferences.jsonl'))
     write_jsonl([item for item in ds['val_chat_records']], Path('val_chat.jsonl'))
 
-
-    launch_code = "poetry run python -m turbo_alignment train_dpo --experiment_settings_path turbo-alignment/tutorials/dpo/dpo.json"
+    launch_code = 'poetry run python -m turbo_alignment train_dpo --experiment_settings_path turbo-alignment/tutorials/dpo/dpo.json'
     subprocess.run(launch_code, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
