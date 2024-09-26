5f2a1ae7085056bd864eb564ed950f966c616f45
lmeribal <e.rykov@tinkoff.ru>
2024-09-26 14:30:07 +0000

    attention pooling adapter

--- turbo_alignment/modeling/multimodal/projectors/__init__.py
+++ turbo_alignment/modeling/multimodal/projectors/__init__.py
@@ -1,3 +1,6 @@
+from turbo_alignment.modeling.multimodal.projectors.attention_pooling import (
+    AttentionPoolingMultiModalProjector,
+)
 from turbo_alignment.modeling.multimodal.projectors.c_abstractor import CAbstractor
 from turbo_alignment.modeling.multimodal.projectors.llava import (
     LlavaMultiModalProjector,
