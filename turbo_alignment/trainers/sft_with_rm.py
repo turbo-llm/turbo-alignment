4208d3dac5448124ccd0c2a55b2d161b7f8b9c02
d.taranets <d.taranets@tinkoff.ru>
2024-10-22 13:35:35 +0300

    lint fix

--- turbo_alignment/trainers/sft_with_rm.py
+++ turbo_alignment/trainers/sft_with_rm.py
@@ -1,15 +1,15 @@
 from typing import Any
+from pathlib import Path
 import os
 
 import torch
 from torch import nn
-from pathlib import Path
 from transformers import PreTrainedModel
 from transformers.trainer_pt_utils import nested_detach
 from transformers.utils import logging
 
-from turbo_alignment.trainers.multigpu import MultiGPUCherryPicksTrainer
 from transformers.trainer_utils import PREFIX_CHECKPOINT_DIR
+from turbo_alignment.trainers.multigpu import MultiGPUCherryPicksTrainer
 
 logger = logging.get_logger(__name__)
 
@@ -20,9 +20,6 @@ class SFTwithRMTrainer(MultiGPUCherryPicksTrainer):
 
         sft_logits = sft_logits.view(-1, sft_logits.size(-1))
         sft_labels = inputs['inputs_w']['input_ids']
-
-        # print(f'INPUTS TEXT SHAPE: {sft_labels.shape}')
-        # print(f'INPUTS TEXT: {sft_labels}')
 
         sft_labels_1 = sft_labels.view(-1)[: reward_token_pos_w[0]]
         sft_labels_2 = sft_labels.view(-1)[reward_token_pos_w[0] + 1 :]
@@ -73,6 +70,20 @@ class SFTwithRMTrainer(MultiGPUCherryPicksTrainer):
         checkpoint_folder = f'{PREFIX_CHECKPOINT_DIR}-{self.state.global_step}'
         run_dir = self._get_output_dir(trial=trial)
         output_dir = Path(os.path.join(run_dir, checkpoint_folder))
+
+        if metrics is not None and self.args.metric_for_best_model is not None:
+            metric_to_check = self.args.metric_for_best_model
+            if not metric_to_check.startswith('eval_'):
+                metric_to_check = f'eval_{metric_to_check}'
+            metric_value = metrics[metric_to_check]
+            operator = np.greater if self.args.greater_is_better else np.less
+            if (
+                self.state.best_metric is None
+                or self.state.best_model_checkpoint is None
+                or operator(metric_value, self.state.best_metric)
+            ):
+                self.state.best_metric = metric_value
+                self.state.best_model_checkpoint = output_dir
 
         (output_dir / 'decoder').mkdir(parents=True, exist_ok=True)
 
