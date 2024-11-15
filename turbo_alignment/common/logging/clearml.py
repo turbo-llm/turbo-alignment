58cc93f495d9fc2d905aa3538ca97a1b3cbf0389
lmeribal <e.rykov@tinkoff.ru>
2024-11-15 09:04:13 +0000

    skip clearml fix

--- turbo_alignment/common/logging/clearml.py
+++ turbo_alignment/common/logging/clearml.py
@@ -7,7 +7,7 @@ from turbo_alignment.settings.logging.clearml import ClearMLSettings
 
 def create_clearml_task(parameters: ClearMLSettings, config: dict[str, Any] | None = None) -> Task:
     clearml_task = Task.init(
-        task_name=parameters.task_name, project_name=parameters.project_name, continue_last_task=True, auto_connect_frameworks=False # FIXME?
+        task_name=parameters.task_name, project_name=parameters.project_name, continue_last_task=True # FIXME?
     )
 
     clearml_task.connect_configuration(config, name='HyperParameters')
