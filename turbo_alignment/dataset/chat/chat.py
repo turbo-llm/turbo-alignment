b38e9d9429d90e4e5f8250b746bc2fbbb135201d
a.khokhulin <a.khokhulin@tinkoff.ru>
2024-11-24 00:38:47 +0300

    dataset records separate

--- turbo_alignment/dataset/chat/chat.py
+++ turbo_alignment/dataset/chat/chat.py
@@ -372,10 +372,11 @@ class InferenceChatDataset(ChatDataset):
             random_cut=self._random_cut,
         )
 
-        new_instance.records = self.records[start:end]
+        dataset_records = [self[idx] for idx in range(len(self))]
 
+        new_instance.records = self.records[start:end]
         new_instance.original_records_map = {
-            record['id']: self.get_original_record_by_id(record['id']) for record in new_instance.records
+            record['id']: self.get_original_record_by_id(record['id']) for record in dataset_records
         }
 
         return new_instance
