ce58a394ee425cae1bc9c1bc4e7d062623237e88
Pavel Geyn <p.geyn@tinkoff.ru>
2024-12-23 10:31:08 +0500

    sft

--- turbo_alignment/modeling/gemma2/patch.py
+++ turbo_alignment/modeling/gemma2/patch.py
@@ -1,8 +1,9 @@
 from transformers.models.gemma2.modeling_gemma2 import GEMMA2_ATTENTION_CLASSES
 
-from .ulysses_attn import Gemma2FlashAttention2Ulysses, Gemma2AttentionUlysses
+from .ulysses_attn import Gemma2FlashAttention2Ulysses, Gemma2AttentionUlysses, Gemma2Attention
 
 
 def patch_gemma_attn_dict():
     GEMMA2_ATTENTION_CLASSES['flash_attention_2_ulysses'] = Gemma2FlashAttention2Ulysses
     GEMMA2_ATTENTION_CLASSES['eager_ulysses'] = Gemma2AttentionUlysses
+    GEMMA2_ATTENTION_CLASSES['eager'] = Gemma2Attention
