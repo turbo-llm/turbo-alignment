a2919f071c1f5ae41ba3d77ef14ce2c276e2142b
Pavel Geyn <p.geyn@tinkoff.ru>
2024-12-24 13:49:24 +0500

    rename and fixes

--- turbo_alignment/sequence_parallel/utils.py
+++ turbo_alignment/sequence_parallel/utils.py
@@ -1,29 +1,13 @@
+import functools
 import os
+
 import torch
 import torch.distributed as dist
 
 
-def create_hook(name: str, output_dir: str = '/mnt/p.geyn/gradients', seq_p_inited: bool = False):
-    def hook(grad):
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir, exist_ok=True)
-
-        middle_name = f'_rank_{dist.get_rank()}' if seq_p_inited else ''
-        shape_filename = os.path.join(output_dir, name + middle_name + '.shape')
-        filename = os.path.join(output_dir, name + middle_name + '.npy.gz')
-
-        with open(shape_filename, 'w') as output:
-            output.write(' '.join(map(str, grad.shape)))
-            output.write('\n')
-            output.write(str(grad.dtype))
-
-        print(f'{dist.get_rank()=} {name=} {grad.min().item()=} {grad.max().item()=}')
-
-        np_grad = grad.float().cpu().numpy()
-        print(f'{dist.get_rank()=} {name=} {np_grad.min().item()=} {np_grad.max().item()=}')
-        np_grad.tofile(filename)
-
-    return hook
+@functools.lru_cache()
+def print_once(msg):
+    print(msg)
 
 
 def write_shape(ar, filename):
@@ -33,16 +17,28 @@ def write_shape(ar, filename):
         output.write(str(ar.dtype))
 
 
-import functools
+def create_hook(name: str, output_dir: str = '/mnt/p.geyn/gradients', seq_p_inited: bool = False):
+    def hook(grad):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir, exist_ok=True)
 
-@functools.lru_cache()
-def print_once(msg):
-    print(msg)
+        middle_name = f'_rank_{dist.get_rank()}' if seq_p_inited else ''
+        shape_filename = os.path.join(output_dir, name + middle_name + '.shape')
+        filename = os.path.join(output_dir, name + middle_name + '.npy')
+
+        write_shape(grad, shape_filename)
+        print(f'{dist.get_rank()=} {name=} {grad.min().item()=} {grad.max().item()=}')
+
+        np_grad = grad.float().cpu().numpy()
+        print(f'{dist.get_rank()=} {name=} {np_grad.min().item()=} {np_grad.max().item()=}')
+        np_grad.tofile(filename)
+
+    return hook
 
 
 def process_forward_output(output_dir, name, middle_name, output, module=None, args=None):
     shape_filename = os.path.join(output_dir, name + middle_name + '.shape')
-    filename = os.path.join(output_dir, name + middle_name + '.npy.gz')
+    filename = os.path.join(output_dir, name + middle_name + '.npy')
     if os.path.exists(filename):
         return
 
@@ -58,30 +54,32 @@ def process_forward_output(output_dir, name, middle_name, output, module=None, a
     print(f'{dist.get_rank()=} {name=} {np_output.min().item()=} {np_output.max().item()=}')
     np_output.tofile(filename)
 
-    if name == 'model.layers.1.input_layernorm':
-        filename = os.path.join(output_dir, name + '_weight_' + middle_name + '.npy')
-        shape_filename = os.path.join(output_dir, name + '_weight_' + middle_name + '.shape')
-        if os.path.exists(filename):
-            return
 
-        write_shape(module.weight, shape_filename)
-        weight = module.weight.float().detach().cpu().numpy()
-        weight.tofile(filename)
-        print(args[0])
+class ForwardHook:
+    def __init__(self, name: str, output_dir: str, seq_p_inited: bool = False):
+        self.name = name
+        self.output_dir = output_dir
+        suff = f'_{dist.get_rank()}' if seq_p_inited else ''
+        self.forward_file = os.path.join(output_dir, f'forward_order{suff}.txt')
+        self.written = False
+        self.middle_name = f'_rank_{dist.get_rank()}' if seq_p_inited else ''
+
+        os.makedirs(self.output_dir, exist_ok=True)
+
+    def __call__(self, module, args, output):
+        if isinstance(output, tuple):
+            for ind, local_output in enumerate(output):
+                if not self.written:
+                    with open(self.forward_file, 'a') as o:
+                        o.write(f'{self.name}_{ind}\n')
+                process_forward_output(self.output_dir, f'{self.name}_{ind}', self.middle_name, local_output, module, args)
+        else:
+            if not self.written:
+                with open(self.forward_file, 'a') as o:
+                    o.write(f'{self.name}\n')
+            process_forward_output(self.output_dir, self.name, self.middle_name, output, module, args)
 
 
 def create_forward_hook(name: str, output_dir: str = '/mnt/p.geyn/forward', seq_p_inited: bool = False):
-    def hook(module, args, output):
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir, exist_ok=True)
-
-        middle_name = f'_rank_{dist.get_rank()}' if seq_p_inited else ''
-        if isinstance(output, tuple):
-            for ind, local_output in enumerate(output):
-                process_forward_output(output_dir, f'{name}_{ind}', middle_name, local_output, module, args)
-        else:
-            process_forward_output(output_dir, f'{name}', middle_name, output, module, args)
-
-    return hook
-
+    return ForwardHook(name, output_dir, seq_p_inited)
 
