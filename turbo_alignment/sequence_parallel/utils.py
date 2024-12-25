2f476e8e309c1febe6013612346efb2633d88161
Pavel Geyn <p.geyn@tinkoff.ru>
2024-12-25 15:02:33 +0500

    test refactor

--- turbo_alignment/sequence_parallel/utils.py
+++ turbo_alignment/sequence_parallel/utils.py
@@ -4,6 +4,8 @@ import os
 import torch
 import torch.distributed as dist
 
+from transformers.modeling_outputs import CausalLMOutput, CausalLMOutputWithPast, CausalLMOutputWithCrossAttentions
+
 
 @functools.lru_cache()
 def print_once(msg):
@@ -36,23 +38,27 @@ def create_hook(name: str, output_dir: str = '/mnt/p.geyn/gradients', seq_p_init
     return hook
 
 
-def process_forward_output(output_dir, name, middle_name, output, module=None, args=None):
+def process_forward_output(output_dir, name, middle_name, output, module=None, args=None) -> bool:
     shape_filename = os.path.join(output_dir, name + middle_name + '.shape')
     filename = os.path.join(output_dir, name + middle_name + '.npy')
     if os.path.exists(filename):
-        return
+        return False
+
+    if isinstance(output, (CausalLMOutput, CausalLMOutputWithCrossAttentions, CausalLMOutputWithPast)):
+        output = output.logits
 
     if not isinstance(output, torch.Tensor):
         print_once(f'have output: {type(output)}')
-        return
+        return False
 
     write_shape(output, shape_filename)
 
-    print(f'{dist.get_rank()=} {name=} {output.min().item()=} {output.max().item()=}')
+    print(f'{dist.get_rank()=} {name=} {output.min().item()=} {output.max().item()=} {output.dtype=}')
 
     np_output = output.float().cpu().detach().numpy()
     print(f'{dist.get_rank()=} {name=} {np_output.min().item()=} {np_output.max().item()=}')
     np_output.tofile(filename)
+    return True
 
 
 class ForwardHook:
@@ -67,19 +73,26 @@ class ForwardHook:
         os.makedirs(self.output_dir, exist_ok=True)
 
     def __call__(self, module, args, output):
+        set_flag = False
         if isinstance(output, tuple):
             for ind, local_output in enumerate(output):
+                if process_forward_output(self.output_dir, f'{self.name}_{ind}', self.middle_name, local_output, module, args):
+                    if not self.written:
+                        with open(self.forward_file, 'a') as o:
+                            o.write(f'{self.name}_{ind}\n')
+
+                        set_flag = True
+        else:
+            if process_forward_output(self.output_dir, self.name, self.middle_name, output, module, args):
                 if not self.written:
                     with open(self.forward_file, 'a') as o:
-                        o.write(f'{self.name}_{ind}\n')
-                process_forward_output(self.output_dir, f'{self.name}_{ind}', self.middle_name, local_output, module, args)
-        else:
-            if not self.written:
-                with open(self.forward_file, 'a') as o:
-                    o.write(f'{self.name}\n')
-            process_forward_output(self.output_dir, self.name, self.middle_name, output, module, args)
+                        o.write(f'{self.name}\n')
+
+                    set_flag = True
+
+        if set_flag:
+            self.written = True
 
 
 def create_forward_hook(name: str, output_dir: str = '/mnt/p.geyn/forward', seq_p_inited: bool = False):
     return ForwardHook(name, output_dir, seq_p_inited)
-
