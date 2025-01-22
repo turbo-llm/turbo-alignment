f56e14ee17acdb960def0822dc2f6ac9ccba7c1f
Pavel Geyn <p.geyn@tinkoff.ru>
2025-01-22 10:36:52 +0500

    fix hook

--- turbo_alignment/sequence_parallel/utils.py
+++ turbo_alignment/sequence_parallel/utils.py
@@ -67,7 +67,7 @@ class ForwardHook:
         self.output_dir = output_dir
         self.suff = f'_{dist.get_rank()}' if seq_p_inited else ''
         self.forward_file = os.path.join(output_dir, f'forward_order{self.suff}.txt')
-        self.written = dist.get_rank() != 0
+        self.written = dist.is_initialized() and dist.get_rank() != 0
         self.middle_name = f'_rank_{dist.get_rank()}' if seq_p_inited else ''
 
         os.makedirs(self.output_dir, exist_ok=True)
@@ -92,8 +92,10 @@ class ForwardHook:
 
         attention_mask = kwargs.get('attention_mask')
         if self.name == 'model' and attention_mask is not None and dist.get_rank() == 0:
-            write_shape(attention_mask, os.path.join(self.output_dir, f'attention_mask{self.suff}.shape'))
-            attention_mask.cpu().numpy().tofile(os.path.join(self.output_dir, f'attention_mask{self.suff}.npy'))
+            shape_file = os.path.join(self.output_dir, f'attention_mask{self.suff}.shape')
+            if not os.path.exists(shape_file):
+                write_shape(attention_mask, os.path.join(self.output_dir, f'attention_mask{self.suff}.shape'))
+                attention_mask.cpu().numpy().tofile(os.path.join(self.output_dir, f'attention_mask{self.suff}.npy'))
 
         if set_flag:
             self.written = True
