9aeb4632ac54e4a83c4c3a600e49f9af7d67abb5
Dmitry Stepakov <dimanata1232@gmail.com>
2025-02-13 13:53:11 +0300

    Update turbo_alignment/settings/tf/vllm.py
    Co-authored-by: Alexey Malakhov <131314005+alekseymalakhov11@users.noreply.github.com>

--- turbo_alignment/settings/tf/vllm.py
+++ turbo_alignment/settings/tf/vllm.py
@@ -7,6 +7,6 @@ class EngineSettings(ExtraFieldsNotAllowedBaseModel):
     dtype: Optional[Literal['auto', 'half', 'float16', 'bfloat16', 'float', 'float32']] = 'auto'
     tensor_parallel_size: int = 1
     gpu_memory_utilization: float | None = 0.9
-    max_logprobs: Optional[int] = None
+    max_logprobs: int | None = None
     enable_lora: bool = False
     max_lora_rank: Optional[int] = None
