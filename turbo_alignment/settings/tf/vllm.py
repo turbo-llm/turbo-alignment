a07d5e158c673f459e9c75e0cf401d3f8334bf89
Dmitry Stepakov <dimanata1232@gmail.com>
2025-02-13 15:33:19 +0300

    Typing

--- turbo_alignment/settings/tf/vllm.py
+++ turbo_alignment/settings/tf/vllm.py
@@ -4,7 +4,7 @@ from turbo_alignment.settings.base import ExtraFieldsNotAllowedBaseModel
 
 
 class EngineSettings(ExtraFieldsNotAllowedBaseModel):
-    dtype: Optional[Literal['auto', 'half', 'float16', 'bfloat16', 'float', 'float32']] = 'bfloat16'
+    dtype: Literal['auto', 'half', 'float16', 'bfloat16', 'float', 'float32'] | None = 'bfloat16'
     tensor_parallel_size: int = 1
     gpu_memory_utilization: float | None = 0.95
     max_logprobs: int | None = None
