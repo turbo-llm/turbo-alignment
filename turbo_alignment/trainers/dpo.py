bda6402367edbbdf823c33695f0cb612409b34af
dan-taranetc <taranetsdan@gmail.com>
2025-02-18 08:02:14 +0000

    return num_items_in_batch

--- turbo_alignment/trainers/dpo.py
+++ turbo_alignment/trainers/dpo.py
@@ -857,6 +857,7 @@ class DPOTrainer(Trainer):
         model: PreTrainedModel | nn.Module,
         inputs: dict[str, torch.Tensor | Any],
         return_outputs=False,
+        num_items_in_batch=None,
     ) -> torch.Tensor | tuple[torch.Tensor, dict[str, float]]:
         loss, metrics = self.get_batch_metrics(model, inputs, train_eval='train')
 
