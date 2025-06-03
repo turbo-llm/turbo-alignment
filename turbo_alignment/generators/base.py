0f7751803bf8863796c6fad7720e8437316e635d
v.p.zelenkovskiy <v.p.zelenkovskiy@tbank.ru>
2025-06-03 23:27:37 +0300

    fix linters

--- turbo_alignment/generators/base.py
+++ turbo_alignment/generators/base.py
@@ -131,14 +131,13 @@ class ChatGeneratorBase(BaseGenerator, Generic[DatasetRecordT, InferenceOutputT]
             if self._transformers_generator_parameters.num_beams != 1:
                 raise ValueError('You can not use batch generation with num_beams != 1')
 
+            self._tokenizer.padding_side = 'left'
             if self._tokenizer.padding_side == 'right':
                 logger.warning(
                     'Changing tokenizer.padding side from "right" to "left".'
                     'This may affect model performance.'
                     'Please verify that `padding_side="left"` is correct for your use case.'
                 )
-
-            self._tokenizer.padding_side = 'left'
             self._tokenizer.pad_token_id = self._tokenizer.pad_token_id
 
             return self._generate_from_batch_records(records, original_records, dataset_name)
